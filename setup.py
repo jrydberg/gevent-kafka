c312eca0d3c7f1d729bcc24235a90da455362aef
Johan Rydberg <johan.rydberg@gmail.com>
2012-04-16 09:49:25 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gevent-kafka',
+      version='0.0',
+      description='ApacheKafka bindings for gevent',
+      author='Johan Rydberg',
+      author_email='johan.rydberg@gmail.com',
+      url='https://github.com/jrydberg/gevent-kafka',
+      packages=['gevent_kafka']
+     )
