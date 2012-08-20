175785751263e82c1afc2ba020507a3afa63e0aa
Johan Rydberg <johan.rydberg@gmail.com>
2012-08-20 08:17:12 +0200

    add partitioner support

--- gevent_kafka/producer.py
+++ gevent_kafka/producer.py
@@ -17,17 +17,34 @@ import random
 from gevent_kafka import broker
 
 
+class NoBrokersAvailableError(Exception):
+    """No brokers available."""
+
+
+class NoPartitionerError(Exception):
+    """No partitioner has been specified for the producer."""
+
+
+NO_KEY = object()
+
+
 class Producer(object):
     """A message producer for a topic.
 
     The producer has to be started using C{start}.
+
+    @ivar partitioner: A callable with signature C{(key, num_parts)}
+        that should return a number between 0 and C{num_parts - 1}
+        that states to which partition the messages with C{key} should
+        be sent.
     """
 
-    def __init__(self, framework, topic):
+    def __init__(self, framework, topic, partitioner=None):
         self.framework = framework
         self.topic_name = topic
         self.brokers = {}
         self.topic_parts = {}
+        self.partitioner = partitioner
 
     def start(self):
         """Start the producer."""
@@ -43,9 +60,46 @@ class Producer(object):
         self.broker_mon.close()
         self.topic_mon.close()
 
-    def send(self, messages):
-        """Send messages to the topic."""
-        broker_id = random.choice(self.brokers.keys())
-        part_no = random.randint(0, self.topic_parts.get(broker_id, 1) - 1)
+    def send(self, messages, key=NO_KEY):
+        """Send messages to the topic.
+
+        @param messages: A sequence of messages that should be
+            published to the topic.
+        @param key: If specified, the partitioner will be used to
+            decide which partition should receive the messages, based
+            on the key.
+
+        @raise NoBrokersAvailableError: If there are no brokers
+            available.
+        @raise NoPartitionerError: If a C{key} was specified, but the
+            producer was created without a partitioner.
+        """
+        if not self.brokers:
+            raise NoBrokersAvailableError()
+
+        broker_id, part_no = self._select_partition(key)
         return self.brokers[broker_id].produce(self.topic_name, part_no,
             messages)
+
+    def _select_partition(self, key):
+        """Given a C{key} return a C{(broker-id, partition-num)} tuple
+        that specifies where messages should be sent.
+        """
+        partitions = [(broker_id, part_id)
+            for broker_id in sorted(self.brokers.keys())
+            for part_id in range(self.topic_parts.get(
+                    broker_id, 1))]
+
+        if key is not NO_KEY:
+            if not self.partitioner:
+                raise NoPartitionerError()
+            key_part_id = self.partitioner(key, len(partitions))
+            if key_part_id < 0 or key_part_id >= len(partitions):
+                raise ValueError(
+                    "returned partition index is without bounds")
+
+            broker_id, part_no = partitions[key_part_id]
+        else:
+            broker_id, part_no = random.choice(partitions)
+
+        return broker_id, part_no
